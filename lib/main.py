3b32c05fd35eec5703a4412eb8b7f58ba81ff578
Tae <abcteapatipol@gmail.com>
2020-05-19 13:45:41 +0000

    end train

--- lib/main.py
+++ lib/main.py
@@ -38,7 +38,7 @@ GENOME_SAVE_NAME = 'winnerLevel1f.pkl'
 
 USING_CHECKPOINT = 1
 FILE_PREFIX = 'checkpoint-level1f-'
-runFile = 'last-level1f-994'
+runFile = 'last-level1f-1484'
 WORKER_NUM = 20
 DUMMY_SCREEN = 1
 GEN_RUN = 500
